76cdfcf0ed45ca9f960a31ac6d390060f656327f
Kristoffer Bråthen Halden <>
2020-03-11 14:47:14 +0100

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Panel from './Panel.js';
 
-export default function App() {
+export default function App(props) {
   return (
     <div className="App">
       <h1>Noroff Task</h1>
+      <Panel>
+        <p>this is the child of the panel function</p>
+        <p>How th fuck is this usefull</p>
+        <div className="Undertekst">
+          <p>Hmmm, maybe there would be nice</p>
+        </div>
+      </Panel>
+      <Panel>
+        <p>This makes it either clear or more confusing</p>
+        <p>oh nice thats what I thought!</p>
+        <h2>Perfect</h2>
+      </Panel>
     </div>
   );
 }
